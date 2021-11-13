bde7652fa4f607e95d3d18da8dfbaee7d990ae1c
yeisson <yeissonpigo@unisabana.edu.co>
2021-11-13 12:01:35 -0500

    onetoone relation between usuario model and user model

--- store/models.py
+++ store/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.db.models.base import Model
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
 
 
 class usuario(models.Model):
+    user = models.OneToOneField(User, on_delete=CASCADE)
     primer_nombre = models.CharField(max_length=120, blank=False)
     segundo_nombre = models.CharField(max_length=120, blank=True)
     primer_apellido = models.CharField(max_length=120, blank=False)
