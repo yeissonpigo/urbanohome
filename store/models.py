f98498b15e19e1af6926c8d9e4a95c3cc2961bc7
yeisson <yeissonpigo@unisabana.edu.co>
2021-11-13 11:53:16 -0500

    app store created. user profile created.

--- store/models.py
+++ store/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.db.models.base import Model
 
 # Create your models here.
+
+
+class usuario(models.Model):
+    primer_nombre = models.CharField(max_length=120, blank=False)
+    segundo_nombre = models.CharField(max_length=120, blank=True)
+    primer_apellido = models.CharField(max_length=120, blank=False)
+    segundo_apellido = models.CharField(max_length=120, blank=True)
+    identificacion = models.IntegerField(max_length=13)
+    correo = models.EmailField()
+    celular = models.IntegerField(max_length=13)
+    confirmado = models.BooleanField()
