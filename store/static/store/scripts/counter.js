285e1603d942764e3fb89ae52462db239cf6436a
yeisson <yeissonpigo@unisabana.edu.co>
2022-01-16 18:22:17 -0500

    guardado button has been added, it is disabled by default, but when the user modifies the quantity, it gets enabled, and the text changes to guardar. Make guardar button an ajax request, and put disabled again is required

--- store/static/store/scripts/counter.js
+++ store/static/store/scripts/counter.js
@@ -1,13 +1,27 @@
 function minus_counter(quantityId) {
-    var myQuantity = document.getElementById('quantity'+quantityId)
+    var myQuantity = document.getElementById('quantity' + quantityId)
     var myValue = Number(myQuantity.value) || 0;
-    if(myValue > 0)
-    myQuantity.value = Number(myValue - 1);
+    if (myValue > 0) {
+        myQuantity.value = Number(myValue - 1);
+        button_on(quantityId);
+    }
 }
 
 function plus_counter(quantityId) {
-    var myQuantity = document.getElementById('quantity'+quantityId)
+    var myQuantity = document.getElementById('quantity' + quantityId)
     var myValue = Number(myQuantity.value) || 0;
-    if(myValue < 20)
-    myQuantity.value = Number(myValue + 1);
+    if (myValue < 20) {
+        myQuantity.value = Number(myValue + 1);
+        button_on(quantityId);
+    }
+}
+
+function button_on(quantityId) {
+    var myButton = document.getElementById('save_cart' + quantityId)
+    update_button(false, 'Guardar', myButton);
+}
+
+function update_button (disable, text, element) {
+    element.disabled = disable;
+    element.firstChild.innerHTML = text;
 }
