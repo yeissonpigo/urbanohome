e86c56aad6d6d41e16f3a70c482d9fc59d20d568
yeisson <yeissonpigo@unisabana.edu.co>
2022-01-31 13:38:25 -0500

    check access on view functions have been added to avoid advanced users to do things they are not supposed to

--- store/views.py
+++ store/views.py
@@ -52,9 +52,12 @@ def login(request):
 
 
 def logout(request):
-    logoutAuth(request)
-    messages.info(request, 'Su sesión se ha cerrado correctamente.')
-    return redirect('register_success')
+    if not request.user.is_authenticated:
+        return HttpResponseForbidden('No tienes acceso a este método.')
+    else:
+        logoutAuth(request)
+        messages.info(request, 'Su sesión se ha cerrado correctamente.')
+        return redirect('register_success')
 
 # register function takes care of registering new users
 # request: Request Object
@@ -63,26 +66,29 @@ def logout(request):
 
 
 def register(request):
-    if request.method == 'GET':
-        user_registration_form = UserRegistrationForm()
-        cliente_registration_form = ClienteRegistrationForm()
+    if request.user.is_authenticated:
+        return HttpResponseForbidden('No tienes acceso a este método.')
     else:
-        user_registration_form = UserRegistrationForm(request.POST)
-        cliente_registration_form = ClienteRegistrationForm(request.POST)
-        print(cliente_registration_form.is_valid(
-        ), cliente_registration_form.errors, type(cliente_registration_form.errors))
-        if user_registration_form.is_valid():
-            user = user_registration_form.save(commit=False)
-            user.password = make_password(user.password)
-            user = user_registration_form.save()
-            cliente_registration_form.instance.user = user
-            print(cliente_registration_form.is_valid())
+        if request.method == 'GET':
+            user_registration_form = UserRegistrationForm()
+            cliente_registration_form = ClienteRegistrationForm()
+        else:
+            user_registration_form = UserRegistrationForm(request.POST)
+            cliente_registration_form = ClienteRegistrationForm(request.POST)
+            print(cliente_registration_form.is_valid(
+            ), cliente_registration_form.errors, type(cliente_registration_form.errors))
+            if user_registration_form.is_valid():
+                user = user_registration_form.save(commit=False)
+                user.password = make_password(user.password)
+                user = user_registration_form.save()
+                cliente_registration_form.instance.user = user
+                print(cliente_registration_form.is_valid())
 
-            if cliente_registration_form.is_valid():
-                cliente = cliente_registration_form.save()
-                return redirect('register_success')
-    return render(request, 'store/register.html', {'cliente_form': cliente_registration_form,
-                                                   'user_form': user_registration_form})
+                if cliente_registration_form.is_valid():
+                    cliente = cliente_registration_form.save()
+                    return redirect('register_success')
+        return render(request, 'store/register.html', {'cliente_form': cliente_registration_form,
+                                                       'user_form': user_registration_form})
 
 # register_success function is mainly a testing function, users will be redirected here when registering, or login
 # request: Request Object
@@ -104,7 +110,7 @@ def show_items(request):
         username = request.user
         user = Cliente.objects.get(user=username.id)
         return render(request, 'store/store.html', {'products': products, 'userId': user.id})
-    return render(request, 'store/store.html', {'products': products,})
+    return render(request, 'store/store.html', {'products': products, })
 
 # card function will check request method, if it's get, will show the form to save a new cart, if already exists doesn't do anything. If request is post, will save the cart if non existant on the database.
 # request: Request Object
@@ -112,72 +118,96 @@ def show_items(request):
 
 
 def card(request):
-    if request.method == 'GET':
-        card_form = CardForm()
+    if not request.user.is_authenticated:
+        return HttpResponseForbidden('No tienes acceso a este método.')
     else:
-        card_form = CardForm(request.POST)
-        if card_form.is_valid():
-            card = card_form.save(commit=False)
-            my_card = Carro.objects.filter(clienteId=card.clienteId, productoId=card.productoId)
-            if my_card:
-                messages.error(request, '¡Ups! Ya tienes este producto en tu carrito de compras. Para revisar tu carrito de compras, click AQUÍ.')
-            else:
-                messages.success(request, '¡Tu producto se ha añadido correctamente!')
-                card = card_form.save()
-            data = send_message(messages.get_messages(request))
-            return HttpResponse(json.dumps(data), content_type="application/json")
-    return render(request, 'store/card_test.html', {'card_form': card_form, })
+        if request.method == 'GET':
+            card_form = CardForm()
+        else:
+            card_form = CardForm(request.POST)
+            if card_form.is_valid():
+                card = card_form.save(commit=False)
+                my_card = Carro.objects.filter(
+                    clienteId=card.clienteId, productoId=card.productoId)
+                if my_card:
+                    messages.error(
+                        request, '¡Ups! Ya tienes este producto en tu carrito de compras. Para revisar tu carrito de compras, click AQUÍ.')
+                else:
+                    messages.success(
+                        request, '¡Tu producto se ha añadido correctamente!')
+                    card = card_form.save()
+                data = send_message(messages.get_messages(request))
+                return HttpResponse(json.dumps(data), content_type="application/json")
+        return render(request, 'store/card_test.html', {'card_form': card_form, })
 
 # card_index function will fetch all the objects from Carro table in database, but if request method is post, will update the card with the new quantity of products
 # request: Request Object
 #return a render of a new page, sending the carro info, or a json response to send confirmation of the request#
 
 
-def  card_index(request):
-    if request.method == 'GET':
-        user = request.user.id
-        my_cliente = Cliente.objects.get(user_id = request.user.id)
-        my_cards = Carro.objects.filter(clienteId = my_cliente.id)
-        my_productos = []
-        for my_card in my_cards:
-            my_product = Producto.objects.get(id = my_card.productoId.id)
-            my_productos.append(my_product)
-    else: 
-        card_form = CardForm(request.POST)
-        if card_form.is_valid():
-            card = card_form.save(commit=False)
-            my_cart = Carro.objects.get(clienteId = card.clienteId, productoId = card.productoId)
-            if my_cart:
-                messages.success(request, '¡Tu carro de compras se ha actualizado!')
-                my_cart.cantidad = card.cantidad
-                my_cart.save()
+def card_index(request):
+    if not request.user.is_authenticated:
+        return HttpResponseForbidden('No tienes acceso a este método.')
+    else:
+        if request.method == 'GET':
+            user = request.user.id
+            my_cliente = Cliente.objects.get(user_id=request.user.id)
+            my_cards = Carro.objects.filter(clienteId=my_cliente.id)
+            my_productos = []
+            for my_card in my_cards:
+                my_product = Producto.objects.get(id=my_card.productoId.id)
+                my_productos.append(my_product)
+        else:
+            card_form = CardForm(request.POST)
+            if card_form.is_valid():
+                card = card_form.save(commit=False)
+                my_cart = Carro.objects.get(
+                    clienteId=card.clienteId, productoId=card.productoId)
+                if my_cart:
+                    messages.success(
+                        request, '¡Tu carro de compras se ha actualizado!')
+                    my_cart.cantidad = card.cantidad
+                    my_cart.save()
+                else:
+                    messages.success(
+                        request, '¡Ups! Ha habido un error, intenta de nuevo.')
+
+                data = send_message(messages.get_messages(request))
+                return HttpResponse(json.dumps(data), content_type="application/json")
+        return render(request, 'store/card.html', {'my_cards': my_cards, 'my_products': my_productos, 'userId': my_cliente.id, 'test': my_cliente
+                                                   })
+
+# delete_cart function will delete certain product from the cart. Is meant for ajax.
+# request: Request object
+# return json object with messages, and required data.
+
+
+def delete_cart(request):
+    if not request.user.is_authenticated:
+        return HttpResponseForbidden('No tienes acceso a este método.')
+    else:
+        if request.method == 'POST':
+            delete_form = DeleteCardForm(data=request.POST)
+            if delete_form.is_valid():
+                my_cart = Carro.objects.get(
+                    clienteId=delete_form.instance.clienteId, productoId=delete_form.instance.productoId)
+                deleted = my_cart.delete()
+                if deleted[0] > 0:
+                    messages.success(
+                        request, 'El producto se ha eliminado de manera satisfactoria')
+                else:
+                    messages.error(
+                        request, 'Lo sentimos, algo ha sucedido. Intenta nuevamente.')
             else:
-                messages.success(request, '¡Ups! Ha habido un error, intenta de nuevo.')
-                
+                messages.error(
+                    request, 'Lo sentimos, hubo un error, por favor intenta nuevamente más tarde.')
             data = send_message(messages.get_messages(request))
             return HttpResponse(json.dumps(data), content_type="application/json")
-    return render(request, 'store/card.html', {'my_cards': my_cards, 'my_products': my_productos, 'userId': my_cliente.id, 'test': my_cliente
-    })
-    
-def delete_cart(request):
-    print('entró a este punto 1')
-    if request.method == 'POST':
-        print('entró a este punto 2')
-        delete_form = DeleteCardForm(data=request.POST)
-        print(delete_form.errors)
-        print(delete_form.is_valid())
-        if delete_form.is_valid():
-            my_cart = Carro.objects.get(clienteId = delete_form.instance.clienteId, productoId = delete_form.instance.productoId)
-            deleted = my_cart.delete()
-            if deleted[0] > 0:
-                messages.success(request, 'El producto se ha eliminado de manera satisfactoria')
-            else:
-                messages.error(request, 'Lo sentimos, algo ha sucedido. Intenta nuevamente.')
-        else:
-            messages.error(request, 'Lo sentimos, hubo un error, por favor intenta nuevamente más tarde.')
-        data = send_message(messages.get_messages(request))
-        return HttpResponse(json.dumps(data), content_type="application/json")
-        
+
+
+# send_message processes the data from messages to prepare it to be sent to ajax request.
+# messages: messages type from django.
+# return dictionary called data.
 def send_message(messages):
     django_messages = []
 
@@ -186,10 +216,10 @@ def send_message(messages):
             "level": message.level,
             "message": message.message,
             "extra_tags": message.tags,
-    })
-        
+        })
+
     data = {}
     data['success'] = 'info'
     data['messages'] = django_messages
-    
+
     return data
